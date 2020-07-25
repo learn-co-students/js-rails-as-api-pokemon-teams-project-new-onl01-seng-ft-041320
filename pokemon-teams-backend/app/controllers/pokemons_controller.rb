806299f26a92fbac89170f374955d01490437433
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-25 15:21:10 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,28 @@
+class PokemonsController < ApplicationController
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons
+    end
+
+    def show
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: pokemon
+    end
+
+    def create
+        trainer = Trainer.find_by(id: params["trainerId"])
+            if trainer.pokemons.count < 6
+                pokemon = Pokemon.create(nickname: Faker::Name.first_name, species: Faker::Games::Pokemon.name, trainer_id: trainer.id)
+                render json: pokemon
+            else
+                render json: {message: 'A Trainer can have a maximum of six Pokemon'}
+            end
+    end
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:id, :nickname, :species, :trainer_id)
+    end
+
+end
